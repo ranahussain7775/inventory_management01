6fa4a53b17b960a47c0de6de4749ea4087cc612b
ranahussain7775 <148821021+ranahussain7775@users.noreply.github.com>
2025-09-06 14:37:46 +0600

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -2,9 +2,9 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mail import Mail # <-- This line is new
+from flask_mail import Mail
 
 # ডাটাবেস, লগইন ম্যানেজার এবং মেইল অবজেক্ট এখানে তৈরি করুন
 db = SQLAlchemy()
 login_manager = LoginManager()
-mail = Mail() # <-- This line is new
+mail = Mail()
